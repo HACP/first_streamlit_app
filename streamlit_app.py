e8612ae9d24c331e4881f8f3c8f05f2844a9185a
HACP <hugo.a.contreras.p@gmail.com>
2023-01-22 12:55:17 -0500

    Update streamlit_app.py
    Cosmetic changes - section and button

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from urllib.error import URLError
 import snowflake.connector
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Snowflake related function
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
